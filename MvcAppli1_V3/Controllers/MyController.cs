4b5561c790049daf3eb588711769ae0e260d70ed
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 16:24:07 +0530

    MvcAppli1_V3

---
+++ MvcAppli1_V3/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppli1_V3.Controllers
+{
+    public class MyController : Controller
+    {
+        //
+        // GET: /My/
+
+        public string Index()
+        {
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+
+    }
+}
---
+++ MvcAppli1_V3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAppli1_V3.MvcApplication" Language="C#" %>
